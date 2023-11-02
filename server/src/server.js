5795322bc8158cd4261b5c7bc4dfb548222c00e0
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-11-02 15:49:08 +0800

    Setting up the server

---
+++ server/src/server.js
@@ -0,0 +1,7 @@
+import app from "./app.js";
+import http from "http";
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer(app);
+server.listen(PORT, () => console.log(`Listening on PORT ${PORT}...`));
