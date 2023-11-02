c49d36ec9877c30804bce16ab2e63cc1e5d640f8
Ryan G. Bitonio <ryanbitonio.ayang@gmail.com>
2023-11-03 01:17:55 +0800

    Adding helmet middleware for security

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 import express from "express";
 import productsRouter from "./routes/products.router.js";
+import helmet from "helmet";
 
 const app = express();
 
 app.use(express.json());
+app.use(helmet());
 
 app.use("/products", productsRouter);
 
